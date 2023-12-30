413a6a449d6ba4e9451106af78616903098e90dc
elfbrtc <elifbarutcuyazilim@gmail.com>
2023-12-30 22:13:21 +0300

    -creating home page design

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.render('index')
+    const locals = {
+        title: "Blog app with NodeJs",
+        description: "Blog app with NodeJS, express, MongoDB and JWT"
+    }
+    res.render('index', { locals})
+})
+
+router.get('/about', (req, res) => {
+    res.render('about')
 })
 
 module.exports = router;
